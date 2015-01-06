65c9fd4a98606e81b64ca78e5fb6ef3317b9675a
Breaux Gramm <breaux@breauxgramm.in>
2015-01-05 17:14:32 -0800

    UI work

--- reddit_oracle/reddit_oracle.js
+++ reddit_oracle/reddit_oracle.js
@@ -5,12 +5,13 @@
 $(document).ready(function() {
     $("#b").click( function() {
 
-        $.getJSON("http://localhost:8080/?callback=?",
+        $.getJSON("http://reddit_oracle_api.breauxgramm.in:8080/?callback=?",
             {
                 t: $("#t_input").val()
             },
             function (data) {
-                    $("#result").text(data);
+                    $("#result-container").prepend($("<div><table><tr><td>Comment:</td><td>" + $("#t_input").val()
+                    + "</td></tr><tr><td>Predicted Karma:</td><td>" + data +"</td></tr></table></div>"));
                 }
 
         );
